53a6b43bc025ceea9ed0b9a60a9def6c2ecfe1de
Kamran Sharif <sharif.kamrna@mail.ru>
2022-07-01 03:15:47 +0300

    gitig

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 import express  from "express";
+import jwt from "jsonwebtoken"
 const app = express();
+app.use(express.json());
 app.get('/' , (req, res) =>{
-res.send('Hello World');
+res.send('пидор Hello World');
 });
+app.post('/auth/login', (req, res) => {
+    console.log(req.body)
+    const token = jwt.sign({
+ email: req.body.email,
+ fullname: 'камашина',
+    }, 
+    '123', 
+    )
+ res.json({
+    parol: true,
+    token,
+ })
+})
 app.listen(1983 , (err) => {
     if(err){
         return console.log(err)
