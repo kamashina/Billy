9b8eb132b788eb1fb1f5622eb3cda60afdc136a1
Kamran Sharif <sharif.kamrna@mail.ru>
2022-07-07 08:43:16 +0300

    rez

--- client/src/Component/Main/Main.jsx
+++ client/src/Component/Main/Main.jsx
@@ -3,9 +3,20 @@ import './Main.css';
 function Main() {
   return (
     <main>
-      <h1>
-        ГЛАВНОЕ
-      </h1>
+      <h1>Резюме</h1>
+      <span>
+        Учусь на втором курсе университета в IT направлении(прикладная информатика).
+        Изучаю Frontend Developer последние 5 месяцев , есть проект на
+        {' '}
+        <a href="https://github.com/kamashina/Billy">github</a>
+        .
+        Навыки: JavaScript, CSS, HTML, redux, React JS, умею работать с git, писать HTTP запросы , имею общее представление о beckend`е и работе с базами данных. Все мои навыки вы можете посмотреть на проекте githhub: https://github.com/kamashina/Billy
+        Рассчитываю на стажировку в направлении
+        Frontend Developer под руководством опытного наставника
+        , с возможностью дальнейшего
+        продвижения, готов уделять на это максимум времени и сил. Зп не важно.
+
+      </span>
     </main>
   );
 } export default Main;
