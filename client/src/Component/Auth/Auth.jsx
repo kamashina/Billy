58237b85a3a676717d16b3f902762ecb08a91d29
Kamran Sharif <sharif.kamrna@mail.ru>
2022-07-08 16:24:53 +0300

    authcss2

--- client/src/Component/Auth/Auth.jsx
+++ client/src/Component/Auth/Auth.jsx
@@ -8,12 +8,10 @@ function Auth() {
   return (
     <div>
       <Header />
-      <div className="container">
-        <Routes>
-          <Route path="client/src/Component/Auth/login/Login.jsx" element={<Login className="login" />} />
-          <Route path="client/src/Component/Auth/reg/Reg.jsx" element={<Reg className="reg" />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="client/src/Component/Auth/login/Login.jsx" element={<Login className="login" />} />
+        <Route path="client/src/Component/Auth/reg/Reg.jsx" element={<Reg className="reg" />} />
+      </Routes>
     </div>
   );
 } export default Auth;
