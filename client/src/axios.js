0574c5bb1e259b01b415602b2873cd15a528c650
Kamran Sharif <sharif.kamrna@mail.ru>
2022-07-12 05:48:44 +0300

    addavatar2

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'https://server.cum.com.ru',
-  baseURL: 'http://localhost:1983',
+  baseURL: 'https://server.cum.com.ru',
   url: 'https://localhost:1983',
 });
 export default instance;
